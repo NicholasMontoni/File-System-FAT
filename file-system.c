313439642e4894bf374e23169b96ddd4abcfcc49
NicholasMontoni <montoni.2021469@studenti.uniroma1.it>
2024-06-28 13:01:21 +0200

    writeFile first implementation

--- file-system.c
+++ file-system.c
@@ -4,6 +4,7 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
 
 #include "file-system.h"
@@ -39,7 +40,6 @@ FileHandle* createFile(FATFileSystem* fs, char *filename) {
     Entry* new_entry = (Entry*) malloc(sizeof(Entry));
     new_entry->name = filename;
     new_entry->start_index = block_found;
-    new_entry->size = 0;
     new_entry->type = TYPE_FILE;
     new_entry->parent = current_dir;
 
@@ -48,19 +48,28 @@ FileHandle* createFile(FATFileSystem* fs, char *filename) {
     if (fs->current_dir->start_index == -1) fs->current_dir->start_index = new_entry->start_index;
 
     FileHandle* entryfh = (FileHandle*) malloc (sizeof (FileHandle));
+    entryfh->name = new_entry->name;
     entryfh->parent_name = current_dir->name;
+    entryfh->last_block = block_found;
     entryfh->pos = 0;
 
     return entryfh;
 }
 
-void eraseFile(FATFileSystem* fs, const char *filename) {
+void eraseFile(FATFileSystem* fs, FileHandle* fh) {
     Entry* current_dir = fs->current_dir;
+
+    if (strcmp(current_dir->name, fh->parent_name) != 0) {
+        printf("The file is not in this directory. Change to '%s' directory and try again\n", fh->parent_name);
+        return;
+    }
+
     for (int i=0; i<current_dir->num_files; i++) {
-        if (strcmp(current_dir->files[i]->name, filename)==0) {
+        if (strcmp(current_dir->files[i]->name, fh->name)==0) {
             int b = current_dir->files[i]->start_index;
             do {
                 int temp = fs->FAT[b];
+                memset(fs->data+(b*BLOCK_SIZE),'\0', BLOCK_SIZE);
                 fs->FAT[b] = FREE;
                 b = temp;
             } while (b != EOF_BLOCK);
@@ -70,13 +79,52 @@ void eraseFile(FATFileSystem* fs, const char *filename) {
             for (int j = i; j < fs->current_dir->num_files - 1; j++) {
                 fs->current_dir->files[j] = fs->current_dir->files[j + 1];
             }
-            current_dir->num_files--; 
+            current_dir->num_files--;
+
+            free(fh);
+
             return;
         }
     }
-    printf("File not found\n");
 }
 
+void writeFile(FATFileSystem* fs, FileHandle *fh, const void *buf, int size) {
+    if (strcmp(fs->current_dir->name, fh->parent_name) != 0) {
+        printf("The file is not in this directory. Change to '%s' directory and try again\n", fh->parent_name);
+        return;
+    }
+
+    if (size < BLOCK_SIZE - (fh->pos % BLOCK_SIZE)) {
+        memcpy (fs->data + (fh->last_block * BLOCK_SIZE), buf, size);
+        fh->pos += size;
+    }
+
+    else {
+        int written_bytes = 0;
+        memcpy (fs->data + (fh->last_block * BLOCK_SIZE), buf, BLOCK_SIZE - (fh->pos % BLOCK_SIZE));
+        written_bytes += (BLOCK_SIZE - (fh->pos % BLOCK_SIZE));
+        fh->pos += written_bytes;
+        size -= written_bytes;
+
+        while (size > BLOCK_SIZE) {
+            for (int i=0; i<NUM_BLOCKS; i++) {
+                if (fs->FAT[i] == FREE) {
+                    fs->FAT[fh->last_block] = i;
+                    fs->FAT[i] = EOF_BLOCK;
+                    fh->last_block = i;
+                    break;
+                }
+            }
+            memcpy (fs->data + (fh->last_block * BLOCK_SIZE), buf + written_bytes, BLOCK_SIZE);
+            fh->pos += BLOCK_SIZE;
+            written_bytes += BLOCK_SIZE;
+            size -= BLOCK_SIZE;
+        }
+
+        memcpy (fs->data + (fh->last_block * BLOCK_SIZE), buf + written_bytes, size);
+        fh->pos += size;
+    }
+}
 
 int main(int argc, char *argv[]) {
     
@@ -84,7 +132,9 @@ int main(int argc, char *argv[]) {
     FATFileSystem* fs = (FATFileSystem*) malloc (sizeof (FATFileSystem));
     memset (fs->FAT, FREE, sizeof(fs->FAT));
     int fd = open ("filesystem", O_RDWR | O_CREAT, 0666);
+    ftruncate(fd, BLOCK_SIZE*NUM_BLOCKS);
     fs->data = mmap(NULL, NUM_BLOCKS*BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    close (fd);
 
     fs->current_dir = (Entry*) malloc (sizeof(Entry));
     fs->current_dir->name = "root";
@@ -97,19 +147,21 @@ int main(int argc, char *argv[]) {
     fs->current_dir->files = (Entry**) malloc(MAX_NUM_FILES * sizeof(Entry*));
 
     FileHandle* f1 = createFile(fs, "file1.txt");
+   
+
+    /*for (int i=0; i<NUM_BLOCKS*3; i++) {
+        printf("%c, ", fs->data[i]);
+    }
+    printf("\n");*/
+    
     FileHandle* f2 = createFile(fs, "file2.txt");
+
+    writeFile(fs, f2, "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", 180);
+
+    
     FileHandle* f3 = createFile(fs, "file3.txt");
 
-    printf("[");
-    for (int i=0; i<NUM_BLOCKS; i++) {
-        printf("%d, ", fs->FAT[i]);
-    }
-    printf("]\n");
-
-    printf("%s\n", f2->parent_name);
-
-    eraseFile(fs, "file2.txt");
-    eraseFile(fs, "file1.txt");
+     writeFile(fs, f1, "questo è il mio primo file ciao a tuttiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", 180);
 
     printf("[");
     for (int i=0; i<NUM_BLOCKS; i++) {
@@ -117,5 +169,16 @@ int main(int argc, char *argv[]) {
     }
     printf("]\n");
 
-    eraseFile(fs, "file4.txt");
+    for (int i=0; i<NUM_BLOCKS*20; i++) {
+        printf("%c, ", fs->data[i]);
+    }
+    printf("\n");
+
+
+     
+    free(fs->current_dir->directories);
+    free(fs->current_dir->files);
+    free(fs->current_dir);
+    munmap(fs->data, NUM_BLOCKS*BLOCK_SIZE);
+    free(fs);
 }
--- file-system.h
+++ file-system.h
@@ -1,6 +1,6 @@
 #define MAX_NUM_FILES 100
 #define NUM_BLOCKS 1024
-#define BLOCK_SIZE 4096
+#define BLOCK_SIZE 40
 #define FREE -1
 #define EOF_BLOCK -2
 #define TYPE_FILE 1
@@ -9,7 +9,6 @@
 typedef struct Entry{
     char* name;
     int start_index;
-    int size;
     int type;
     int num_files;
     int num_directories;
@@ -25,15 +24,17 @@ typedef struct {
 } FATFileSystem;
 
 typedef struct {
+    char* name;
     char* parent_name;
+    int last_block;
     int pos;
 } FileHandle;
 
 FileHandle *createFile(FATFileSystem* fs, char *filename);
-void eraseFile(FATFileSystem* fs, const char *filename);
-void writeFile(FileHandle *fh, const void *buf, int size);
-void readFile(FileHandle *fh, void *buf, int size);
-void seekFile(FileHandle *fh, int pos);
+void eraseFile(FATFileSystem* fs, FileHandle* fh);
+void writeFile(FATFileSystem* fs, FileHandle *fh, const void *buf, int size);
+void readFile(FATFileSystem* fs, FileHandle *fh, void *buf, int size);
+void seekFile(FATFileSystem* fs, FileHandle *fh, int pos);
 void createDir(FATFileSystem* fs, const char *dirname);
 void eraseDir(FATFileSystem* fs, const char *dirname);
 void changeDir(FATFileSystem* fs, const char *dirname);
